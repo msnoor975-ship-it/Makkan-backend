85dcc8153db5c17622b239299ce1866d00f9fbdc
BaraaArram <baraaarram2001@gmail.com>
2026-07-06 11:58:46 +0300

    Fix asyncHandler import path in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const asyncHandler = require('../utils/asyncHandler');
+const { asyncHandler } = require('../middleware/errorHandler');
 const {
   getAllUsers,
   getUserById,
